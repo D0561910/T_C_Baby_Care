306341342399e8436e89c8b0e0f25cdf074d9bfe
D0561910 <d0561910@mail.fcu.edu.tw>
2018-05-03 16:25:11 +0800

    babycare

---
+++ app/src/main/java/com/example/charlessin/baby_care/ShopActivity.java
@@ -0,0 +1,13 @@
+package com.example.charlessin.baby_care;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+    }
+}
